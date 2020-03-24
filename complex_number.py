9018c84837b6d45f8958352c13d7be1c896c9c8e
mtfaye <tahafaye@hotmail.com>
2020-03-24 01:58:44 -0400

    add complex_number file

---
+++ complex_number.py
@@ -0,0 +1,28 @@
+# Create a complex number class
+
+class Complex(object):
+     def __init__(self, real, imag):
+          self.real = real
+          self.imag = imag
+
+
+     def __add__(self, other):
+          return Complex(self.real + other.real,
+                         self.imag + other.imag)
+
+     def __sub__(self, other):
+          return Complex(self.real - other.real,
+                         self.imag - other.imag)
+
+     def __multi__(self, other):
+          return Complex(self.real * other.real,
+                         self.imag * other.imag)
+
+     def __str__(self):
+          return '<'+str(self.real)+ ',' +str(self.imag)+'>'
+
+
+U = Complex(2, -1)
+V = Complex(1, 0)
+W = U * V
+print(W)
