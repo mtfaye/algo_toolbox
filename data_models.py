aa832327c993abb7edbd9e138cb027f5155cb18c
mtfaye <tahafaye@hotmail.com>
2020-04-28 13:02:03 -0400

    add database

---
+++ data_models.py
@@ -0,0 +1,30 @@
+# Python talk - James Powell
+# So you want to be A Python Expert?
+
+# Data Models
+
+class Polynomial:
+    def __init__(self):
+        self.coeffs =coeffs
+    
+    def __repr__(self):
+        return 'Polynomial(*(!r)' .format(self.coeffs)
+
+    def __add__(self, other):
+        return Polynomial(*(x +y for x, y in zip(self.coeffs,other.coeffs)))
+
+    def __len__(self):
+        return len(self.coeffs)
+
+    def __call__(self):
+        pass
+
+
+# Generators
+# Top level syntax, function -> underscore methods
+# x ()
+
+def add(x, y):
+    return x + y
+
+
