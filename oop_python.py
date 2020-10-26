bad6106d573682af17202dc23421c231dde85193
mtfaye <tahafaye@hotmail.com>
2020-10-26 07:41:08 -0400

    Update oop_python.py

--- oop_python.py
+++ oop_python.py
@@ -1,3 +1,19 @@
+# New Class for Filter Prism 
 
+class NewClassFilterPrism:
+  pass
+  def __init__(self):
+    pass
+  def first_function(self):
+    pass
+  def second_function(self):
+    pass
+  
+  
+  
+def main():
+  pass
 
+if __name__ == main():
+  pass
 
