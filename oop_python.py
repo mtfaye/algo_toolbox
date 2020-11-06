646740d1387b5386ccb016b91f3e05980388d2bc
mtfaye <tahafaye@hotmail.com>
2020-11-05 21:08:21 -0500

    Update oop_python.py

--- oop_python.py
+++ oop_python.py
@@ -2,3 +2,11 @@
 # Files for good oop good practices 
 # use this filed for PR only.
 
+# practice decorators here !
+
+
+def some_function(func):
+  def inner_function(func):
+    return something 
+  return inner_func
+
