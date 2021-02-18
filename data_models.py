7cab0aaee8c6680c60ae3582a45fe9ed7e203264
mtfaye <tahafaye@hotmail.com>
2021-02-17 21:33:23 -0500

    Update data_models.py

--- data_models.py
+++ data_models.py
@@ -1,6 +1,7 @@
 # Python talk - James Powell
-# So you want to be A Python Expert?
+# So you want to be a Python Expert?
 # Data Models
+
 
 class Polynomial:
     def __init__(self):
@@ -29,5 +30,3 @@ class MetaData:
     class Inner_Call(MetaData):
         def__init__(self):
             pass
-        
-        
