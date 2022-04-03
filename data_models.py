02af2b13cfae83d44375bcf53fe6c43b5e0bf082
mtfaye <tahafaye@hotmail.com>
2022-04-03 12:23:17 -0400

    Update data_models.py

--- data_models.py
+++ data_models.py
@@ -9,7 +9,10 @@ class Polynomial:
         return 'Polynomial(*(!r)' .format(self.coeffs)
 
     def __add__(self, other):
-        return Polynomial(*(x +y for x, y in zip(self.coeffs,other.coeffs)))
+        return Polynomial(*(x + y for x, y in zip(self.coeffs,other.coeffs)))
 
     def __len__(self):
         return len(self.coeffs)
+    
+    def __str__(self):
+        return str(self.coeffs)
