035da6c2070f71104f62416688ddbddb37e00c52
mtfaye <tahafaye@hotmail.com>
2022-05-23 08:30:25 -0400

    Update 121.BestTimetoBuyandSellStock.py

--- 121.BestTimetoBuyandSellStock.py
+++ 121.BestTimetoBuyandSellStock.py
@@ -1,3 +1,6 @@
+# using extra memory space:
+# Space = O(n)
+# Time = O(n)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         ans = []
@@ -8,3 +11,20 @@ class Solution:
                 anchor = i
             ans.append(prices[i] - prices[anchor])
         return max(ans)
+
+
+# Space = O(1)
+# Time = O(n)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        anchor = 0
+        max_profit = 0
+        
+        for i in range(len(prices)):
+            profit = prices[i] - prices[anchor]
+            
+            if profit <= 0:
+                anchor = i
+            max_profit = max(max_profit, profit)
+            
+        return max_profit
